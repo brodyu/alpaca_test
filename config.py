a5f7e4d2ef6ec64bfa7ba0354a74133e70796c57
Brody <brodyuehara@gmail.com>
2020-07-22 14:34:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-#authentication and connection details
+#authentication and connection details ahsdoiahsdoiahsodih
 API_KEY = 'PKJA4FK19MW5RW239JOL'
 API_SECRET = 'BGP9VmVwUPKnXY5vqpBdAUSZn6HHxe9JWNuTKhX/'
